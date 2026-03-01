e1896d97289592faa115ba0c382cdcf00d9c686d
Rudraksh sachdev <rudrakshsachdeva04@gmail.com>
2026-03-01 19:07:51 +0530

    feat: Add Java examples for Converting Temperature from Celsius to Farenheit

---
+++ ConvertTemp.java
@@ -0,0 +1,10 @@
+public class ConvertTemp {
+    public static void main(String[] args) {
+        double celsius = 25;
+
+        double farenheit = (celsius * 9/5) + 32;
+
+        System.out.println("Celsius: " + celsius);
+        System.out.println("Farenheit: " + farenheit);
+    }
+}
