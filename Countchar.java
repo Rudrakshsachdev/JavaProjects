b6bc3120ea4d345b3a6e305d5a65d2fdbdfd2a02
Rudraksh sachdev <rudrakshsachdeva04@gmail.com>
2026-03-07 22:04:30 +0530

    Counting characters

---
+++ Countchar.java
@@ -0,0 +1,17 @@
+public class Countchar {
+    public static void main(String[] args) {
+        int count = 0;
+        int overallCount = 0;
+        String str = "Hello World";
+
+        for (int i = 0; i < str.length(); i++) {
+            overallCount++;
+            if (str.charAt(i) == 'l') {
+                count++;
+            }
+        }
+
+        System.out.println("The count of 'l' in the string is: " + count);
+        System.out.println("The overall count of characters in the string is: " + overallCount);
+    }
+}
