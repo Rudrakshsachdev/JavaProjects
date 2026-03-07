d2df495f6e29e5b1d08ff2bc1aae7c3dc211e1aa
Rudraksh sachdev <rudrakshsachdeva04@gmail.com>
2026-03-07 20:43:34 +0530

    program for counting vowels and consonants

--- CountVowels.java
+++ CountVowels.java
@@ -1,13 +1,20 @@
+// program for counting vowels and consonants
+
+
 public class CountVowels {
     public static void main(String[] args) {
         int count = 0;
-        
+
         String vowels = "aeiouAEIOU";
 
-        String str = "Hello World";
+        String test = "Hello World!";
 
-        for (int i = 0; i < str.length(); i++) {
-            if (vowels.contains(str.charAt(i))) {
+
+        //  this for loop will run till the length of the string and check if the character is a vowel or not
+        for (int i = 0; i < test.length(); i++) {
+
+            // this if block will check if the character is a vowel or not
+            if (vowels.contains(String.valueOf(test.charAt(i)))) {
                 count++;
             }
         }
