7f0101ab66ad24e21f9a8792e57736f777ef4892
Rudraksh sachdev <rudrakshsachdeva04@gmail.com>
2026-04-09 23:50:26 +0530

    docs: update program description for character counting functionality

--- Countchar.java
+++ Countchar.java
@@ -15,3 +15,5 @@ public class Countchar {
         System.out.println("The overall count of characters in the string is: " + overallCount);
     }
 }
+
+// This program counts the number of occurrences of the character 'l' in the string "Hello World" and also counts the total number of characters in the string. The results are printed to the console.
